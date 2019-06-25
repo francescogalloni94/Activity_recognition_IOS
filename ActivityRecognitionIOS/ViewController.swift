4f86217dba5a91a097bd1ddb4ae6577d9853a28c
francescoGalloni94 <francesco.galloni94@gmail.com>
2019-06-25 15:54:30 +0200

    initial commit

---
+++ ActivityRecognitionIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityRecognitionIOS
+//
+//  Created by Francesco Galloni on 25/06/2019.
+//  Copyright © 2019 Francesco Galloni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
