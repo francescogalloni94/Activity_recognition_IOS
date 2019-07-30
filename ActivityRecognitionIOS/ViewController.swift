d8518376f92d7331408efda152d28f15dbb35000
francescoGalloni94 <francesco.galloni94@gmail.com>
2019-07-30 16:22:49 +0200

    main view

--- ActivityRecognitionIOS/ViewController.swift
+++ ActivityRecognitionIOS/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let transfrom = CGAffineTransform.init(scaleX: 3.5, y: 3.5)
+        activityIndicator.transform = transfrom
+        activityIndicator.isHidden = true
     }
 
-
+    @IBAction func onButtonClick(_ sender: UIButton) {
+        let buttonString = recordingButton.titleLabel!.text
+        if buttonString == "START" {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        recordingLabel.text = "RECORDING ACTIVITY..."
+        recordingButton.setTitle("STOP",for:.normal)
+        }else if buttonString == "STOP"{
+            
+        }
+        
+    }
+    
 }
 
