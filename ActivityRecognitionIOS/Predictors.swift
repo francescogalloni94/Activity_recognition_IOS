2f860b9606176510beab7f35067b379b9fa32f23
francescoGalloni94 <francesco.galloni94@gmail.com>
2019-08-02 18:25:01 +0200

    class to wrap core ml models

--- ActivityRecognitionIOS/Predictors.swift
+++ ActivityRecognitionIOS/Predictors.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Predictors {
+    
+    var neural_network: nn_ar
+    var random_forets : rf_ar
+    var svm : svm_ar
+    
+    init() {
+        neural_network = nn_ar()
+        self.random_forets = rf_ar()
+        self.svm = svm_ar()
+    }
+    
+    func getNeuralNetworkPredictions(inputs:[nn_arInput])->[nn_arOutput]{
+        guard let output = try? self.neural_network.predictions(inputs:inputs) else {
+            fatalError("Unexpected runtime error.")
+        }
+        return output
+    }
+    
+    func getRandomForestPrediction(inputs:[rf_arInput])->[rf_arOutput]{
+        guard let output = try? self.random_forets.predictions(inputs:inputs) else {
+            fatalError("Unexpected runtime error.")
+        }
+        return output
+    }
+    
+    func getSVMPrediction(inputs:[svm_arInput])->[svm_arOutput]{
+        guard let output = try? self.svm.predictions(inputs:inputs) else {
+            fatalError("Unexpected runtime error.")
+        }
+        return output
+    }
+    
+}
