c4d23a845c0ba5e8c981b14fb219639ee5f66893
francescoGalloni94 <francesco.galloni94@gmail.com>
2019-08-12 16:05:43 +0200

    personal model

--- ModelPrototyping/ActivityRecognitionModel.py
+++ ModelPrototyping/ActivityRecognitionModel.py
@@ -14,12 +14,14 @@ from keras.models import Model
 from keras.wrappers.scikit_learn import KerasClassifier
 from confusionMatrix import plot_confusion_matrix
 import coremltools
+from imblearn.over_sampling import SMOTE
 
 pwd_x_train = '../ActivityRecognitionDataset/UCI HAR Dataset/train/Inertial Signals'
 pwd_y_train = '../ActivityRecognitionDataset/UCI HAR Dataset/train/y_train.txt'
 pwd_x_test = '../ActivityRecognitionDataset/UCI HAR Dataset/test/Inertial Signals'
 pwd_y_test = '../ActivityRecognitionDataset/UCI HAR Dataset/test/y_test.txt'
 pwd_labels = '../ActivityRecognitionDataset/UCI HAR Dataset/activity_labels.txt'
+pwd_myData = '../ActivityRecognitionDataset/MyData/'
 
 print("Reading data...\n")
 body_acc_x_train = pd.read_csv(pwd_x_train+'/body_acc_x_train.txt',delim_whitespace=True,header=None).values
@@ -91,13 +93,11 @@ total_acc_features_test = total_acc_features_test.extractFeature()
 X_test = np.concatenate((body_acc_features_test,body_gyro_features_test,total_acc_features_test),axis=1)
 print(X_test.shape)
 
-print("shuffling data and new train test division...\n")
-X = np.concatenate((X_train,X_test))
-y = np.concatenate((y_train,y_test))
-print(X.shape)
-print(y.shape)
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,shuffle=True)
+#Copy for later training on personal data
+X_train_myData = X_train
+X_test_myData = X_test
+y_train_myData = y_train
+y_test_myData = y_test
 
 print('Feature scaling...\n')
 feature_scaler = StandardScaler()
@@ -185,5 +185,111 @@ nn_model.fit(x=X_train,y=y_train_nn,epochs=50,batch_size=100)
 
 nn_coreml_model = coremltools.converters.keras.convert(nn_model)
 nn_coreml_model.save('nn_ar.mlmodel')
+
+
+print("Reading personal data...\n")
+
+body_acc_x_myData = []
+body_acc_y_myData = []
+body_acc_z_myData = []
+
+body_gyro_x_myData = []
+body_gyro_y_myData = []
+body_gyro_z_myData = []
+
+total_acc_x_myData = []
+total_acc_y_myData = []
+total_acc_z_myData = []
+
+files = ['WALKING','STANDING','LAYING','SITTING','WALKING_U1','WALKING_U2','WALKING_U3','WALKING_D1','WALKING_D2','WALKING_D3']
+count = 0
+
+for name in files:
+    if count == 0:
+        body_acc_x_myData = pd.read_csv(pwd_myData + name + 'XBodyAcc.csv', header=None).values
+        body_acc_y_myData = pd.read_csv(pwd_myData + name + 'YBodyAcc.csv', header=None).values
+        body_acc_z_myData = pd.read_csv(pwd_myData + name + 'ZBodyAcc.csv', header=None).values
+
+        body_gyro_x_myData = pd.read_csv(pwd_myData + name + 'XGyro.csv', header=None).values
+        body_gyro_y_myData = pd.read_csv(pwd_myData + name + 'YGyro.csv', header=None).values
+        body_gyro_z_myData = pd.read_csv(pwd_myData + name + 'ZGyro.csv', header=None).values
+
+        total_acc_x_myData = pd.read_csv(pwd_myData + name + 'XTotalAcc.csv', header=None).values
+        total_acc_y_myData = pd.read_csv(pwd_myData + name + 'YTotalAcc.csv', header=None).values
+        total_acc_z_myData = pd.read_csv(pwd_myData + name + 'ZTotalAcc.csv', header=None).values
+
+        y_temp = pd.read_csv(pwd_myData + name + 'Labels.csv', delim_whitespace=True, header=None).values
+        y_myData = np.ravel(y_temp)
+
+
+    else:
+        body_acc_x_myData = np.concatenate((body_acc_x_myData,pd.read_csv(pwd_myData+name+'XBodyAcc.csv',header=None).values),axis=0)
+        body_acc_y_myData = np.concatenate((body_acc_y_myData,pd.read_csv(pwd_myData+name+'YBodyAcc.csv',header=None).values),axis=0)
+        body_acc_z_myData = np.concatenate((body_acc_z_myData,pd.read_csv(pwd_myData+name+'ZBodyAcc.csv',header=None).values),axis=0)
+
+        body_gyro_x_myData = np.concatenate((body_gyro_x_myData,pd.read_csv(pwd_myData+name+'XGyro.csv',header=None).values),axis=0)
+        body_gyro_y_myData = np.concatenate((body_gyro_y_myData,pd.read_csv(pwd_myData+name+'YGyro.csv',header=None).values),axis=0)
+        body_gyro_z_myData = np.concatenate((body_gyro_z_myData,pd.read_csv(pwd_myData+name+'ZGyro.csv',header=None).values),axis=0)
+
+        total_acc_x_myData = np.concatenate((total_acc_x_myData,pd.read_csv(pwd_myData+name+'XTotalAcc.csv',header=None).values),axis=0)
+        total_acc_y_myData = np.concatenate((total_acc_y_myData,pd.read_csv(pwd_myData+name+'YTotalAcc.csv',header=None).values),axis=0)
+        total_acc_z_myData = np.concatenate((total_acc_z_myData,pd.read_csv(pwd_myData+name+'ZTotalAcc.csv',header=None).values),axis=0)
+
+        y_temp = pd.read_csv(pwd_myData+name+'Labels.csv',delim_whitespace=True,header=None).values
+        y_myData = np.concatenate((y_myData,np.ravel(y_temp)),axis=0)
+
+    count = count+1
+
+print("Feature extraction personal data...\n")
+body_acc_features_myData = SensorFeaturesExtractor(body_acc_x_myData, body_acc_y_myData, body_acc_z_myData)
+body_acc_features_myData = body_acc_features_myData.extractFeature()
+
+body_gyro_features_myData = SensorFeaturesExtractor(body_gyro_x_myData, body_gyro_y_myData, body_gyro_z_myData)
+body_gyro_features_myData = body_gyro_features_myData.extractFeature()
+
+total_acc_features_myData = SensorFeaturesExtractor(total_acc_x_myData, total_acc_y_myData, total_acc_z_myData)
+total_acc_features_myData = total_acc_features_myData.extractFeature()
+
+X_myData = np.concatenate((body_acc_features_myData, body_gyro_features_myData, total_acc_features_myData), axis=1)
+print(X_myData.shape)
+
+print("Oversampling minority classes on personal data...\n")
+X_myData, y_myData = SMOTE().fit_resample(X_myData, y_myData)
+
+
+X_train_myData = np.concatenate((X_train_myData,X_myData),axis=0)
+print(X_train_myData.shape)
+y_train_myData = np.concatenate((y_train_myData,y_myData),axis=0)
+
+
+print('Feature scaling personal data...\n')
+feature_scaler_myData = StandardScaler()
+X_train_myData = feature_scaler_myData.fit_transform(X_train_myData)
+X_test_myData = feature_scaler_myData.transform(X_test_myData)
+
+coreml_scaler_personal = coremltools.converters.sklearn.convert(feature_scaler_myData)
+coreml_scaler_personal.save('feature_scaler_personal.mlmodel')
+
+print("Random Forest with personal data grid search cross validation training...")
+rf_parameters_personal = [{'n_estimators':[10,50,100,150,200,250,300],'criterion':['entropy','gini']}]
+rf_classifier_personal = GridSearchCV(estimator=RandomForestClassifier(),param_grid=rf_parameters_personal,scoring='f1_weighted',cv=5,n_jobs=-1)
+rf_classifier_personal = rf_classifier_personal.fit(X_train_myData,y_train_myData)
+print('Best parameters value: '+str(rf_classifier_personal.best_params_)+'\n')
+print('Best scores on 5-Fold cross validation: '+str(rf_classifier_personal.best_score_)+'\n')
+y_pred_rf_personal = rf_classifier_personal.predict(X_test_myData)
+y_test_rf_personal,y_pred_rf_personal = get_labels(labels_mapping,y_test_myData,y_pred_rf_personal)
+cm_rf_personal = confusion_matrix(y_test_rf_personal,y_pred_rf_personal)
+print('Confusion Matrix:\n')
+print(cm_rf_personal)
+plot_confusion_matrix(cm_rf_personal,filename='RFPersonal_cm.png',title='Random Forest Activity Recognition Personal data')
+print('Accuracy score: '+str(accuracy_score(y_test_rf_personal,y_pred_rf_personal)))
+print('F1 score: '+str(f1_score(y_test_rf_personal,y_pred_rf_personal,average='weighted'))+'\n')
+
+rf_coreml_model_personal = coremltools.converters.sklearn.convert(rf_classifier_personal.best_estimator_)
+rf_coreml_model_personal.save('rf_ar_personal.mlmodel')
+
+
+
+
 
 
